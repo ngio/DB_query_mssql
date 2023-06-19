d9890806dc97708da1a7315c366cd1bc63e55025
wani <mill01@naver.com>
2023-06-19 16:10:18 +0900

    Create SET_IDENTITY_INSERT.sql

---
+++ SET_IDENTITY_INSERT.sql
@@ -0,0 +1,52 @@
+/*SET IDENTITY_INSERT(Transact-SQL)
+
+https://learn.microsoft.com/ko-kr/sql/t-sql/statements/set-identity-insert-transact-sql?view=sql-server-ver16
+
+명시적 값을 테이블의 ID 열에 삽입할 수 있도록 합니다.
+
+
+*/
+USE AdventureWorks2012;  
+GO  
+-- Create tool table.  
+CREATE TABLE dbo.Tool(  
+   ID INT IDENTITY NOT NULL PRIMARY KEY,   
+   Name VARCHAR(40) NOT NULL  
+);  
+GO  
+-- Inserting values into products table.  
+INSERT INTO dbo.Tool(Name)   
+VALUES ('Screwdriver')  
+        , ('Hammer')  
+        , ('Saw')  
+        , ('Shovel');  
+GO  
+  
+-- Create a gap in the identity values.  
+DELETE dbo.Tool  
+WHERE Name = 'Saw';  
+GO  
+  
+SELECT *   
+FROM dbo.Tool;  
+GO  
+  
+-- Try to insert an explicit ID value of 3;  
+-- should return an error:
+-- An explicit value for the identity column in table 'AdventureWorks2012.dbo.Tool' can only be specified when a column list is used and IDENTITY_INSERT is ON.
+INSERT INTO dbo.Tool (ID, Name) VALUES (3, 'Garden shovel');  
+GO  
+-- SET IDENTITY_INSERT to ON.  
+SET IDENTITY_INSERT dbo.Tool ON;  
+GO  
+  
+-- Try to insert an explicit ID value of 3.  
+INSERT INTO dbo.Tool (ID, Name) VALUES (3, 'Garden shovel');  
+GO  
+  
+SELECT *   
+FROM dbo.Tool;  
+GO  
+-- Drop products table.  
+DROP TABLE dbo.Tool;  
+GO  
