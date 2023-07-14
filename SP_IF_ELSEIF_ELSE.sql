42eaca95f3fdb4c4b49ea5a9604b6f4631acc518
wani <mill01@naver.com>
2023-07-14 10:28:20 +0900

    Create SP_IF_ELSEIF_ELSE.sql
     IF…ELSE IF…ELSE

---
+++ SP_IF_ELSEIF_ELSE.sql
@@ -0,0 +1,17 @@
+-- https://simplesqltutorials.com/if-elseif-else/
+
+
+--  IF…ELSE IF…ELSE 
+
+IF(<condition is true>)
+BEGIN
+  <execute some code>
+END
+ELSE IF(<different condition is true>)
+BEGIN
+  <execute some other code>
+END
+ELSE
+BEGIN
+  <execute some other other code>
+END
